5d09eda3e767260166c3272f42a83f4c5ba5509e
PHU KOTA GORONTALO <phukotagorontalo1@gmail.com>
2025-06-23 09:08:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCxUzF4YAu1GEqdHtEg_RXPmZ6IBIBPYI8",
   authDomain: "plhut-43c49.firebaseapp.com",
   projectId: "plhut-43c49",
-  storageBucket: "plhut-43c49.appspot.com",
+  storageBucket: "plhut-43c49.firebasestorage.app",
   messagingSenderId: "976882934933",
   appId: "1:976882934933:web:806593a93ff6bef193e79c",
   measurementId: "G-55KHCK4E8C"
